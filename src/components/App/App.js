2aae27ad8ffe2fad65d628f0723d5159f87b1004
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2022-04-26 05:24:24 +0300

    profile done

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import { Profile } from 'components/Profile/Profile';
+
+import user from '../../data/user.json';
+
+export function App() {
+  return (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+}
