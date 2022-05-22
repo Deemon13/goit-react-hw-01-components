0b8071f92fc5e881d45c04d5418712850ec8b320
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2022-05-22 11:13:07 +0300

    added global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { Normalize } from 'components/GlobalStyle/Normalize';
+import { GlobalStyle } from 'components/GlobalStyle/GlobalStyle';
+import { theme } from 'constants/theme';
 import { App } from 'components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Normalize />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
